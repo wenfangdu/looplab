9c437072df5c45187c8d03f238b45d7f316bd9ab
Leon <849995097@qq.com>
2017-09-06 19:16:52 +1200

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-sm bg-dark nav-dark fixed-top">
+      <div class="container">
+        <a href="index.html" class="navbar-brand navbar-brand--white">BS1</a>
+      </div>
+    </nav>
+  </div>
+
+</template>
+<script>
+  const $ = require("jquery")
+  window.jQuery = $;
+  require("bootstrap")
+
+</script>
+<style>
+  @import "bootstrap";
+  @import "./assets/css/main.css";
+</style>
